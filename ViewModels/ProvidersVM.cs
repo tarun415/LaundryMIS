88f71487a4e8328fe48c6684fc816fb1bf547151
himanshi.mishra@appventurez.com <himanshi.mishra@appventurez.com>
2026-04-24 17:28:44 +0530

    commit By Himanshi Mishra Provider Master Works

---
+++ ViewModels/ProvidersVM.cs
@@ -0,0 +1,14 @@
+﻿namespace LaudaryMis.ViewModels
+{
+    public class ProvidersVM
+    {
+        public int ProviderId { get; set; } = 0;
+        public string? ProviderName { get; set; }
+        public string? FirmName { get; set; }
+        public int? RatePerBed { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
