c257c3f67f1951ac166643e458cb4faf38675f64
dell <dell@DESKTOP-O9BIQM6>
2026-04-27 10:41:45 +0530

    done

--- Repositories/Interfaces/IProviderRepository.cs
+++ Repositories/Interfaces/IProviderRepository.cs
@@ -1,9 +1,16 @@
 ﻿using LaudaryMis.Models;
+using LaudaryMis.ViewModels;
 
 namespace LaudaryMis.Repositories.Interfaces
 {
     public interface IProviderRepository
     {
+        Task DeleteAsync(int id);
         Task<IEnumerable<Provider>> GetAll();
+        Task<IEnumerable<ProvidersVM>> GetProviderAsync();
+        Task<ProvidersVM> GetProviderByIdAsync(int id);
+        Task InsertAsync(ProvidersVM model);
+        Task SaveAsync(ProvidersVM model);
+        Task UpdateAsync(ProvidersVM model);
     }
 }
